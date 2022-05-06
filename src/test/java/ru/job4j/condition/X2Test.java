4ae27270a0d68e07d0de1e8bff5269c39e7bce80
Lambere <krutysh.super@yandex.ru>
2022-05-06 20:59:50 +0300

    1.1. Что такое import

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -14,47 +14,55 @@ public class X2Test {
         int c = 0;
         int x = 2;
         int expected = 40;
-        int rsl = X2.calc(a,b,c,x);
+        int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
+
     @Test
-  public void  Pom()  {
+  public void  calc1()  {
     int a = 1;
     int b = 1;
     int c = 1;
     int x = 1;
     int expected1 = 3;
-    int rsl1 = X2.calc(a,b,c,x);
+    int rsl1 = X2.calc(a, b, c, x);
      Assert.assertEquals(expected1, rsl1);
-}
+    }
+
     @Test
-    public void  NePom()  {
+    public void  calc2()  {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
         int expected1 = 2;
-        int rsl1 = X2.calc(a,b,c,x);
+        int rsl1 = X2.calc(a, b, c, x);
         Assert.assertEquals(expected1, rsl1);
     }
+
     @Test
-    public void  Pom1()  {
+    public void  calc3()  {
         int a1 = 1;
         int b1 = 1;
         int c1 = 0;
         int x1 = 1;
         int expected1 = 2;
-        int rsl1 = X2.calc(a1,b1,c1,x1);
+        int rsl1 = X2.calc(a1, b1, c1, x1);
         Assert.assertEquals(expected1, rsl1);
     }
+
     @Test
-    public void  Pom2()  {
+    public void  calc4()  {
         int a1 = 1;
         int b1 = 1;
         int c1 = 1;
         int x1 = 0;
         int expected1 = 1;
-        int rsl1 = X2.calc(a1,b1,c1,x1);
+        int rsl1 = X2.calc(a1, b1, c1, x1);
         Assert.assertEquals(expected1, rsl1);
+    }
+
+    @Test
+    public void testCalc() {
     }
 }
