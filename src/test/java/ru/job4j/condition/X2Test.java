22b72840fd7ff37c130771a1b08216547cfd5189
Lambere <krutysh.super@yandex.ru>
2022-06-29 11:30:56 +0300

    1.1. Что такое import

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -5,62 +5,47 @@ import org.junit.Test;
 
 public class X2Test {
 
-    @Test
-    public void calc() {
-        int a = 10;
-        int b = 0;
-        int c = 0;
-        int x = 2;
-        int expected = 40;
+@Test
+public void  whenA1B1C1X1Then3()  {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
-    }
+        }
 
-    @Test
-  public void  calc1()  {
-    int a = 1;
-    int b = 1;
-    int c = 1;
-    int x = 1;
-    int expected1 = 3;
-    int rsl1 = X2.calc(a, b, c, x);
-     Assert.assertEquals(expected1, rsl1);
-    }
-
-    @Test
-    public void  calc2()  {
+@Test
+public void  whenA0B1C1X1Then2()  {
         int a = 0;
         int b = 1;
         int c = 1;
         int x = 1;
-        int expected1 = 2;
-        int rsl1 = X2.calc(a, b, c, x);
-        Assert.assertEquals(expected1, rsl1);
-    }
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+        }
 
-    @Test
-    public void  calc3()  {
-        int a1 = 1;
-        int b1 = 1;
-        int c1 = 0;
-        int x1 = 1;
-        int expected1 = 2;
-        int rsl1 = X2.calc(a1, b1, c1, x1);
-        Assert.assertEquals(expected1, rsl1);
-    }
+@Test
+public void  whenA1B1C0X1Then2()  {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+        }
 
-    @Test
-    public void  calc4()  {
-        int a1 = 1;
-        int b1 = 1;
-        int c1 = 1;
-        int x1 = 0;
-        int expected1 = 1;
-        int rsl1 = X2.calc(a1, b1, c1, x1);
-        Assert.assertEquals(expected1, rsl1);
-    }
-
-    @Test
-    public void testCalc() {
-    }
+@Test
+public void  whenA1B1C1X0Then1()  {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 1;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+   }
 }
