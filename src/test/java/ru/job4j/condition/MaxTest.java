1152ee3646d38392acdaca61ccf032ae13c29725
Lambere <krutysh.super@yandex.ru>
2022-07-02 16:05:08 +0300

    4.2. Тернарное сравнение [#189 #297924]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -3,10 +3,7 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MaxTest {
-
     @Test
     public void whenMax1To2Then2() {
         int left = 1;
@@ -17,20 +14,11 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax3To2Then3() {
-        int left = 3;
-        int right = 2;
+    public void whenMax2To1Then2() {
+        int left = 2;
+        int right = 1;
         int result = Max.max(left, right);
-        int expected = 3;
-        Assert.assertEquals(result, expected);
-    }
-
-    @Test
-    public void whenMax4To7Then7() {
-        int left = 4;
-        int right = 7;
-        int result = Max.max(left, right);
-        int expected = 7;
+        int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
