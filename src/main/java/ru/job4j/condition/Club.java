130547be7c375fb73aca6b0d8e164bede9270e0c
Lambere <krutysh.super@yandex.ru>
2022-07-05 20:53:09 +0300

    4.3.1. Сокращенный оператор && [#333783 #298154]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -1,20 +1,18 @@
- package ru.job4j.condition;
+package ru.job4j.condition;
 
-    public class Club {
-        public static void permission(boolean hasMoney, boolean beFriend) {
-            boolean orBoolean = hasMoney || beFriend;
-            if (orBoolean) {
-                System.out.println("I can go to the club.");
-            } else {
-                System.out.println("I can't.");
-            }
-        }
-
-        public static void main(String[] args) {
-            permission(true, true);
-            permission(true, false);
-            permission(false, true);
-            permission(false, false);
+public class Club {
+    public static void permission(boolean hasMoney, boolean beFriend) {
+        if (hasMoney || beFriend) {
+            System.out.println("I can go to the club.");
+        } else {
+            System.out.println("I can't.");
         }
     }
 
+    public static void main(String[] args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+    }
+}
