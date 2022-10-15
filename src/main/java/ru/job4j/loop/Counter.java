5fbe223355a63a345b6c5d6b121dd0446038c5d3
Lambere <krutysh.super@yandex.ru>
2022-10-15 23:16:16 +0300

    Сумма чисел

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,20 +3,15 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int variable = start; variable <= finish; variable++) {
-            sum = variable + sum;
+        for (int index = start; index <= finish;index++) {
+            sum = sum + index;
         }
         return sum;
     }
 
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for ( int variable = start; variable <= finish; variable++){
-            if (variable % 2 == 0){
-                sum = sum += variable;
-            }
-        }
-        return sum;
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
     }
-
 }
