cf3ec486c7cc63d2daa04449f46d25613398a34b
Lambere <krutysh.super@yandex.ru>
2022-10-15 23:03:59 +0300

    LoopFor

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -1,10 +1,8 @@
 package ru.job4j.loop;
-
-public class LoopFor {
-    public static void main(String[] args) {
-        for(int i = 5;i <=10;i++) {
-            System.out.println(i);
+public class LoopFor{
+public static void main(String[] args) {
+        int i;
+        for (i = 5; i <= 10; i++) {
+        System.out.println(i);
         }
-
-    }
-}
+        }}
