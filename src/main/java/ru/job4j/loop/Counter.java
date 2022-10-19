d3626f8ee8435877264bd3e894a565457b5cc3b6
Lambere <krutysh.super@yandex.ru>
2022-10-19 21:11:31 +0300

    Counter

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,8 +3,21 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (int index = start; index <= finish;index++) {
-            sum = sum + index;
+        for (int variable = start; variable <= finish; variable++) {
+            sum = variable + sum;
+            return sum;
+
+        }
+        return sum;
+    }
+
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int variable = start; variable <= finish; variable++) {
+
+            if (variable % 2 == 0)  {
+                sum = sum + variable ;
+            }
         }
         return sum;
     }
@@ -13,5 +26,9 @@ public class Counter {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
