b70eb53cb0ebfe61d691c795bad41ef0569e5f0c
Мегамозг <krutysh.super@yandex.ru>
2022-12-12 22:54:49 +0300

    g

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -4,9 +4,7 @@ public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int variable = start; variable <= finish; variable++) {
-            sum = variable + sum;
-            return sum;
-
+            sum += variable;
         }
         return sum;
     }
@@ -14,7 +12,6 @@ public class Counter {
     public static int sumByEven(int start, int finish) {
         int sum = 0;
         for (int variable = start; variable <= finish; variable++) {
-
             if (variable % 2 == 0)  {
                 sum = sum + variable ;
             }
