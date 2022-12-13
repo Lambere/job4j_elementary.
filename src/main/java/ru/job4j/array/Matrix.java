4eb6d89e5aa70c7629be39486cfc24da1a7a1242
Мегамозг <krutysh.super@yandex.ru>
2022-12-13 15:35:16 +0300

    Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -4,8 +4,7 @@ public class Matrix {
         int[][] siz = new int[size][size];
          for (int cell = 0; cell < size; cell++) {
              for (int index = 0; index < size; index++) {
-                int indexZero =  cell + 1;
-                siz[cell][index] = (index + 1) * indexZero;
+                siz[cell][index] = (index + 1) * (cell + 1);
              }
          }
         return siz;
