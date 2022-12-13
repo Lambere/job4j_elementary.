a89c72eab3124868cf3dc025d4b88101485b1240
Мегамозг <krutysh.super@yandex.ru>
2022-12-13 18:19:19 +0300

    фыв

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -4,7 +4,6 @@ public class MathFunction {
     public static double sum(double first, double second) {
         return first + second;
     }
-
     public static double multiply(double first, double second) {
         return first * second;
     }
