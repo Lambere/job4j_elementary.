4bd69b4331f6487f8934f17f6a01c9367218b41f
Мегамозг <krutysh.super@yandex.ru>
2022-12-14 08:52:34 +0300

    Counter

--- src/main/java/ru/job4j/array/ArrayLoop.java
+++ src/main/java/ru/job4j/array/ArrayLoop.java
@@ -6,6 +6,7 @@ public class ArrayLoop {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = index * 2 + 3;
         }
+
         for (int index : numbers) {
             System.out.println(index);
         }
