dbd73a08b144a84740475093a7240ec96c158a60
Мегамозг <krutysh.super@yandex.ru>
2022-12-14 10:28:21 +0300

    Сумма чисел. [#226416]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,23 +9,10 @@ public class Counter {
         return sum;
     }
 
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        for (int variable = start; variable <= finish; variable++) {
-            if (variable % 2 == 0)  {
-                sum = sum + variable ;
-            }
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
 
-        System.out.println(sumByEven(0, 10));
-        System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
     }
 }
