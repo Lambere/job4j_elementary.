3b386647d1572242431c8729ecc95088a19f3ac5
Мегамозг <krutysh.super@yandex.ru>
2022-12-15 18:24:50 +0300

    Простое число [#171694]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = true;
-        for (int index = 2; index <= number - 1;index++) {
-            if(number % index == 0) {
+        for (int index = 1; index <= number; index++) {
+            if (number % 2 == 0 || number == 1) {
                  prime = false;
                  break;
             }
-        }
-        if (number == 1){
-            prime = false;
         }
         return prime;
     }
