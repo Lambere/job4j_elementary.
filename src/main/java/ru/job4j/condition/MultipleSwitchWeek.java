ea358ca1c3a59bc7576cf33716a009876e19af7d
Мегамозг <krutysh.super@yandex.ru>
2022-12-18 16:15:44 +0300

    Switch - matcher [#334071 #371041]

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -4,7 +4,7 @@ public class MultipleSwitchWeek {
 
     public static int numberOfDay(String name) {
         int value = switch (name) {
-            default -> -1;
+
             case "Понедельник", "Monday" -> 2;
             case "Вторник", "Tuesday" -> 3;
             case "Среда", "Wednesday" -> 4;
@@ -12,7 +12,7 @@ public class MultipleSwitchWeek {
             case "Пятница", "Friday" -> 6;
             case "Суббота", "Saturday" -> 7;
             case "Воскресенье", "Sunday" -> 8;
-
+            default -> -1;
         };
         return value;
     }
