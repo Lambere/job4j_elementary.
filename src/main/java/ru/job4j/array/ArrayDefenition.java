e2c18c00a1b39a5172fcdbb10b7ce9fd91dabf40
Мегамозг <krutysh.super@yandex.ru>
2022-12-19 09:15:06 +0300

    Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefenition.java
+++ src/main/java/ru/job4j/array/ArrayDefenition.java
@@ -3,20 +3,8 @@ package ru.job4j.array;
 public class ArrayDefenition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
         String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
-        String[] names = new String[4];
-        names[0] = "Попка дурак";
-        names[1] = "Или нет?";
-        names[2] = "Или нет?";
-        names[3] = "Или нет?";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
 
     }
 }
