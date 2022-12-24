52f4f9406f2454a9b85c9a1f9dd7d8eabb2eeb75
Мегамозг <krutysh.super@yandex.ru>
2022-12-24 22:28:35 +0500

    Перевернуть массив. [#226790 #372602]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -4,10 +4,9 @@ public class Turn {
 
     public static int[] back(int[] array) {
         for (int index = 0; index < array.length / 2; index++) {
-            int hukke = (array.length - 1) - index;
             int temp = array[index];
-            array[index] = array[hukke];
-            array[hukke] = temp;
+            array[index] = array[(array.length - 1) - index];
+            array[(array.length - 1) - index] = temp;
         }
         return array;
     }
