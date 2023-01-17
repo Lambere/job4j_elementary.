2818729fad2330cbce2d3f0924818a08a1b3ca9f
Мегамозг <krutysh.super@yandex.ru>
2023-01-17 10:56:32 +0500

    Моно столбец в матрице. [#214127 #376390]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -14,12 +14,33 @@ public class MatrixCheck {
 
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
-        for (int i = 0; i <= board.length; i++) {
+        for (int i = 0; i < board.length; i++) {
                 if (board[i][column] != 'X') {
                     result = false;
                     break;
-
                 }
+        }
+        return result;
+    }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
+        return rsl;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length - 1; i++) {
+            for (int row = 0; row < board.length - 1; row++) {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    result = true;
+                    break;
+                }
+            }
+
         }
         return result;
     }
