447772d7bfaf2ddfebbf92f81717556c61971c6f
Пользователь <krutysh.super@yandex.ru>
2023-11-19 22:27:03 +0100

    Ra

--- src/main/java/ru/job4j/array/LengthArrayArrays.java
+++ src/main/java/ru/job4j/array/LengthArrayArrays.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class LengthArrayArrays {
+public class LengthArrayArrays  {
 
     public static void main(String[] args) {
         int[][] array = {{1}, {1, 2}, {1, 2, 3}, {1, 2, 3, 4}};
