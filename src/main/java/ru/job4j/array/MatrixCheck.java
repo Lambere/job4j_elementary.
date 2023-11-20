5b0ad510bf2c679873344f478a3ab247a17b9a68
Пользователь <krutysh.super@yandex.ru>
2023-11-20 08:24:56 +0100

    Ra

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -33,7 +33,6 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-
         for (int i = 0; i < board.length - 1; i++) {
             for (int a = 0; a < board.length - 1; a++) {
                 if (monoHorizontal(board, i) || monoVertical(board, i)) {
