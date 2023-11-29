90ba6d975816df5b4c0d765d992861bbf24c93c3
Пользователь <krutysh.super@yandex.ru>
2023-11-29 22:08:12 +0100

    Ra

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MatrixCheckTest {
+
     @Test
     public void whenDataMonoByTrueThenTrue() {
         char[][] input = {
