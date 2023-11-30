c2d424a47c1b66810c104acd26fcdf1df945f34d
Пользователь <krutysh.super@yandex.ru>
2023-11-30 19:50:22 +0100

    [#53859 #461171].

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,6 +30,7 @@ public class MatrixCheck {
 
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
+
         for (int i = 0; i < board.length; i++) {
             rsl[i] = board[i][i];
         }
