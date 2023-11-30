fcdfd85a2f54991a80898756146f574993b05442
Пользователь <krutysh.super@yandex.ru>
2023-11-30 19:45:20 +0100

    [#53859 #461171]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -39,9 +39,9 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length - 1; i++) {
-            if (monoHorizontal(board, i) || monoVertical(board, i)) {
-                    result = true;
-                    break;
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
                 }
         }
         return result;
