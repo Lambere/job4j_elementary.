bf4c6bd79770a6f9c98348feeaf1e713624dab06
Пользователь <krutysh.super@yandex.ru>
2023-12-03 00:03:06 +0100

    [#22].

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,13 +2,16 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-
         for(int i = 0; array.length >= i;i++) {
-            if(i == array.length) { break;}
+            if(i == array.length) {
+                break;
+            }
             for(int j = i + 1; array.length >= j;j++) {
-                if(j == array.length) { break;}
+                if(j == array.length) {
+                    break;
+                }
                 if(array[i] + array[j] == target) {
-               return new int[]{i,j};
+                    return new int[]{i,j};
                 }
             }
         }
