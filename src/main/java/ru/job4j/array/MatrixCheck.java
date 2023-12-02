c43d4edad01ae93202ea8b981aca601d9525490f
Пользователь <krutysh.super@yandex.ru>
2023-12-02 23:58:02 +0100

    [#225].

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
-
         for (int cell = 0; cell < board.length; cell++) {
             if (board[row][cell] != 'X') {
                 return false;
