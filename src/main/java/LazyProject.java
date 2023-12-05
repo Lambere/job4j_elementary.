ee946e92d76fb2ed94ecbc7333c8d2076e977c9a
Пользователь <krutysh.super@yandex.ru>
2023-12-05 08:44:29 +0100

    [34]

--- src/main/java/LazyProject.java
+++ src/main/java/LazyProject.java
@@ -1,2 +1,16 @@
+import java.util.stream.Stream;
+
 public class LazyProject {
+    public static boolean getXO (String str) {
+        int i = 0;
+        int j = 0;
+        Stream<String> a = Stream.of(str);
+        if(a.allMatch(x -> x.equals("x")) ) {
+            i++;
+        }
+        if(a.allMatch(x -> x.equals("o")) ) {
+            j++;
+        }
+        return i == j;
+    }
 }
