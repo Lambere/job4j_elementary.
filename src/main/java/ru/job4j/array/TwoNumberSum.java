594dcc79d0497449ff4bfcf00ea860027aa401b4
Пользователь <krutysh.super@yandex.ru>
2023-12-05 14:49:29 +0100

    [34]sad

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -5,19 +5,17 @@ public class TwoNumberSum {
         int j = 1;
         int i = 0;
         int[] array1;
-        while (j < array.length ||i == array.length) {
-                    if (array[i] + array[j] == target) {
-                        array1 = new int[]{i, j};
-                        return array1;
-                    } else {
-                        if(j == array.length - 2) {
-                            i++;
-                            j = 0;
-                        }
-
-                        j++;
-                    }
-
+        while (j < array.length || i == array.length) {
+            if (array[i] + array[j] == target) {
+                array1 = new int[]{i, j};
+                return array1;
+            } else {
+                if (j == array.length - 2) {
+                    i++;
+                    j = 0;
+                }
+                j++;
+            }
         }
         return new int[0];
     }
