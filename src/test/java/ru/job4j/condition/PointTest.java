eb1d67ec21c25d3ad905df8cd808fdafe47c2fa4
Пользователь <krutysh.super@yandex.ru>
2023-12-24 09:41:43 +0100

    1.9

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,28 +8,28 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        double out = new Point(0,0).distance(new Point(2,0));
+        double out = new Point(0, 0).distance(new Point(2, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when40to40then2() {
         double expected = 0;
-        double out = new Point(4,0).distance(new Point(4,0));
+        double out = new Point(4, 0).distance(new Point(4, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00to22then2() {
         double expected = 2;
-        double out = new Point(2,0).distance(new Point(2,2));
+        double out = new Point(2, 0).distance(new Point(2, 2));
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when30to80then2() {
         double expected = 5;
-        double out = new Point(3,0).distance(new Point(8,0));
+        double out = new Point(3, 0).distance(new Point(8, 0));
         Assert.assertEquals(expected, out, 0.01);
     }
 }
