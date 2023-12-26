b729000d2464f3b732c4ec787c2c6a0f0634fa7b
Пользователь <krutysh.super@yandex.ru>
2023-12-26 09:13:20 +0100

    3

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,6 +1,38 @@
 package ru.job4j.condition;
 public class Max {
+    public static int max(int left, int right, int a) {
+        int res = 0;
+        if (left > right && left > a) {
+            res =  left;
+        }
+        if (right > left && right > a) {
+            res =  right;
+        }
+        if (a > right && a > left) {
+            res =  a;
+        }
+        return res;
+    }
+
+    public static int max(int left, int right, int a, int b) {
+        int res = 0;
+        if (left > right && left > a && left > b) {
+            res = left;
+        }
+        if (right > left && right > a && right > b) {
+            res = right;
+        }
+        if (b > right && b > a && b > left) {
+            res = b;
+        }
+        if (a > right && a > b && a > left) {
+            res = a;
+        }
+        return res;
+    }
+
+
     public static int max(int left, int right) {
-        return left > right ? left : right;
+        return Math.max(left, right);
     }
 }
